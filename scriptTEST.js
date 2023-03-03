cae7ffa284e1267f9236d8bcc81ae26613e3cf43
Dhruv Lal <100739809+20323dl@users.noreply.github.com>
2023-03-03 07:38:50 +0000

    bullet shoots alien and deletes and moves towrds cir FIXED - ACTUALLY MOVES TOWRDS CIR              -  ALIEN SPAWNS EVERY 7SEC

--- scriptTEST.js
+++ scriptTEST.js
@@ -12,35 +12,66 @@ const TASKNAME = "t01_create_sprite";
 // }
 /****************************IMG***************************/
 
+/****************************setup()***************************/
 function setup() {
   console.log("setup: bob");
-  
   cnv = new Canvas(windowWidth, windowHeight);
-  cir = new Sprite(windowWidth / 2, windowHeight / 2, 75, 75, 'd');
+  //player
+  cir = new Sprite(windowWidth / 2, windowHeight / 2, 75, 'd');
   cir.rotationSpeed = 0;
   cir.shapeColor = color("red")
   cir.stroke = color("red")
+  //player
+
   /** IMG **/
   //cir.addImage(img);
   //img.resize(150, 150)
   /** IMG **/
+
+  //functions
   movement();
   wallGroup = new Group();
   walls();
   alienGroup = new Group();
-  alien();
+  //alien();
+  setInterval(alien, 5000);
   bullet = new Sprite(cir.x, cir.y, 10, 'd');
   bullet.remove();
+  //functions
+}
+/****************************setup()***************************/
+
+/****************************draw()***************************/
+function draw() {
+  background('#ceddf5');
+  cir.collides(wallGroup, bounceWall);
+  cir.rotateTo(mouse, 50);
+  bullet.collides(wallGroup, delBullet)
+  bullet.collides(alienGroup, delAlien)
+  bullet.friction = 9;
+  for (const alien of alienGroup) {
+    alien.moveTo(cir.x, cir.y);
+    alien.setSpeed(2);
+  }
 }
 
-function delAlien(cir, alien) {
-  alien.remove();
-}
-function delWall(cir, wall) {
+/****************************draw()***************************/
+
+//colliding functions
+function bounceWall(cir, wall) {
   cir.vel.x = 0;
   cir.vel.y = 0;
-  //cir.remove();
 }
+function delBullet(wallGroup, bullet) {
+  wallGroup.remove();
+}
+function delAlien(alienGroup, bullet) {
+  bullet.remove();
+  alienGroup.remove();
+}
+//colliding functions
+
+//player move
 function movement() {
   let speed = 40;
   let slow = 0;
@@ -67,19 +98,23 @@ function movement() {
     }
   });
 }
+//player move
+
+//aliens
 function alien() {
-  for (i = 0; i < 5; i++) {
-    alien = new Sprite(random(1, windowWidth), random(1, windowHeight), 80, 80);
+  for (i = 0; i < 7; i++) {
+    alien = new Sprite(random(1, windowWidth), random(1, windowHeight), 80, 80, 'd');
     alien.shapeColor = color("black");
     alien.vel.x = random(1, 10);
     alien.vel.y = random(1, 10);
     alien.bounciness = 0;
-    alien.friction = 0;
-    alien.moveTowards(cir);
-    alien.setSpeed(2)
+    alien.friction = 100;
     alienGroup.add(alien);
   }
 }
+//aliens
+
+//walls
 function walls() {
   let w = 30
   wallLH = new Sprite(0, height / 2, w, height, 'k');
@@ -96,34 +131,14 @@ function walls() {
   wallGroup.add(wallBot);
 
 }
-/*******************************************************/
-// draw()
-/*******************************************************/
-function draw() {
-  background('#ceddf5');
-  //cir = new Sprite(300, 300, 300);
-  cir.collides(wallGroup, delWall);
-  //cir.moveTo(mouseX, mouseY, 1000);
-  cir.rotateTo(mouse, 50);
-  //bullet.collides(wallgroup)
-  bullet.collides(wallGroup, delBullet)
-  bullet.collides(alienGroup, delAlien1)
-  
-  
-}
+//walls
 
-function delBullet(wallGroup, bullet) {
-  wallGroup.remove();  
-}
-
-function delAlien1(alienGroup, bullet) {
-  bullet.remove();  
-  alienGroup.remove();
-}
+//shooting mech
 function mouseClicked() {
   // Create the bullet sprite
   bullet = new Sprite(cir.x, cir.y, 10, 'd');
   bullet.shapeColor = color("red");
+
   // Move the bullet to the edge of the gun and set it's rotation
   setBulletPosition(cir, bullet)
   // Fire the bullet!
@@ -140,7 +155,7 @@ function setBulletPosition(_gun, _round) {
   deg = _gun.rotation;
   rads = deg * Math.PI / 180;  // Convert degrees to radians
 
-  h = _gun.w / 2 + _round.w / 2 +10; // h is the distance from the center of the _gun to the center of the _round
+  h = _gun.w / 2 + _round.w / 2 + 10; // h is the distance from the center of the _gun to the center of the _round
   offsetX = h * Math.cos(rads);
   offsetY = h * Math.sin(rads);
 
@@ -148,6 +163,7 @@ function setBulletPosition(_gun, _round) {
   _round.x = _gun.x + offsetX;
   _round.y = _gun.y + offsetY;
 }
+//shooting mech
 
 /*******************************************************/
 //  END OF APP
